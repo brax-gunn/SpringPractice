f0f9fb862fe4f7fc22520bac873c96b3f9a7b543
Brax Gunn <brax.gunn.99@gmail.com>
2022-12-14 14:06:20 +0530

    aop-handson

--- aop-handson/src/main/java/com/braxgunn/ConnectivityAspect.java
+++ aop-handson/src/main/java/com/braxgunn/ConnectivityAspect.java
@@ -1,5 +1,9 @@
 package com.braxgunn;
 
+import java.time.Duration;
+import java.time.Instant;
+
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
@@ -9,8 +13,16 @@ import org.springframework.stereotype.Component;
 public class ConnectivityAspect {
 
     @Around("@annotation(com.braxgunn.ConnectivityAspectInf)")
-    public void checkConn(){
+    public void checkConn(ProceedingJoinPoint  joinPoint) throws Throwable{
         System.out.println("[CONNECTIVITY]: You are connected to the system");
+        Instant start = Instant.now();
+        
+        joinPoint.proceed();
+        
+        Instant end = Instant.now();
+        long timeElapsed = Duration.between(start, end).toMillis();
+        
+        System.out.println("[INFO]: Took " + timeElapsed + " milliseconds");
     }
     
 }
